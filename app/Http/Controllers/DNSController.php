c404c36f96fba376b1ac4ca4a7ab590a76f5f10b
Anugrah Pratama <angrhprtmh@gmail.com>
2020-02-26 10:46:05 +0700

    update htaccess, procfile, controller

--- app/Http/Controllers/DNSController.php
+++ app/Http/Controllers/DNSController.php
@@ -17,43 +17,49 @@ class DNSController extends Controller
             'domain'    => [ 'required','regex:/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/']
         ]);
 
-         
-
         $data_req = $request->input();
         $id = $data_req['domain']; 
 
+        $keyCache = "DNS";
+        $value = $id;
        
+        $get_cache = Cache::get($keyCache);
         
-        $dns = new Dns($id);
-        // // // $dns = new Spatie\Dns\Dns('google.com');
+        if(!empty($get_cache)){
 
-        $dns->getRecords(); // returns all available dns records
+        // $dns = new Dns($id);
+        // // // // $dns = new Spatie\Dns\Dns('google.com');
 
-        dd($dns);
-    
-        // $data = dns_get_record($id, DNS_A + DNS_AAAA + DNS_MX + DNS_NS + DNS_SOA);   
-        // // $data = dns_get_record($id, DNS_ALL);   
+        // $dns->getRecords(); // returns all available dns records
+
+        // dd($dns);
+
+        $data = dns_get_record($get_cache, DNS_A + DNS_AAAA + DNS_MX + DNS_NS + DNS_SOA);   
+        // $data = dns_get_record($id, DNS_ALL);   
         // return response()->json($data);
-        // $d = json_encode($data);
-        // $e = json_decode($d);
-        // print_r($e);
+        echo "udah ada cache";
+        
+           
+        }else{
+            $value_cache = Cache::add($keyCache, $value, now()->addMinutes(5));
+            $get_cache = Cache::get($keyCache);
 
+            // $dns = new Dns($id);
+            // // // // $dns = new Spatie\Dns\Dns('google.com');
+
+            // $dns->getRecords(); // returns all available dns records
+
+            // dd($dns);
+
+                $data = dns_get_record($get_cache, DNS_A + DNS_AAAA + DNS_MX + DNS_NS + DNS_SOA);   
+                // $data = dns_get_record($id, DNS_ALL);   
+                // return response()->json($data);
+                echo "buat cache baru";
+        }
+     
         // 731ecec5deacc035e3472a3c49c4f0c438e561f410b58da8a7f7345158319f40 (no docker pas docker compose)
         
     }
 
-    // public function check_cache(){
-    //     $keyCache = "Ariel";
-    //     $value = "ini isinya";
-    //     $value_cache = Cache::add($keyCache, $value, now()->addMinutes(5));
 
-    //     dd($value_cache);
-    // }
-
-    // public function look_cache(){
-    //     $keyCache = "Ariel";
-    //     $value_cache = Cache::get($keyCache);
-    //     dd($value_cache);
-    // }
-    
 }
