c4e7fcdda23e6b6c553e3fca117a77677ec64737
Anugrah Pratama <angrhprtmh@gmail.com>
2020-02-26 11:28:07 +0700

    update controller

--- app/Http/Controllers/DNSController.php
+++ app/Http/Controllers/DNSController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Spatie\Dns\Dns;
+use Spatie\Dns\Exceptions\CouldNotFetchDns;
+use Spatie\Dns\Exceptions\InvalidArgument;
 
 class DNSController extends Controller
 {
